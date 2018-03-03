5de1b1ae6dcbdcbe5a2ef832c69695028d1fa628
Alejandro Sanchez <aalejo@gmail.com>
2018-03-03 23:00:09 +0000

    optimizing emit logic

--- README.md
+++ README.md
@@ -1,17 +1,74 @@
-# React Flux Dash
+# React FluxDash
 
-Learning flux is hard, hopefully it will become easier with this library.
+Learning flux is hard, using it is cumbersome. Hopefully it will become easier with this library.
 
-1) 90% of the dispatcher logic is managed on the background
+1) 99% of the dispatcher logic is managed on the background
+2) Easy store managment,
+3) Full MVC approach.
 
-2) New ""
+## Dispatching actions
+
+You actions object needs to inhering from Flex.Action, then you will be able to dispatch to any store and setter
+```
+class SessionActions extends Flux.Action{
+    
+    autenticateUser(){
+        this.dispatch('MyStore.setAutentication', data);
+        // you will have to create a _setAutentication inside StoreActions
+    }
+```
+
+On your store you will be going the following
+
+```
+class StoreActions extends Flux.Store{
+    constructor(){
+        super();
+        this.state = {
+            autenticated: false 
+        }
+    }
+    
+    _setAutentication(){
+        //set the the new store state and emit
+        this.setStoreState({ autenticated: true }).emit();
+        //you can specify an event name if you want
+        this.setStoreState({ autenticated: true }).emit('EVENT_NAME');
+    }
+}
+```
+## Handling store changes
+
+There are 3 ways of going it
 
 1) New lifecycle component function **handleStoreChanges** for store changes
+
 ```
-    handleStoreChanges(){
-        // suposing you have imported a UserStore into your react component
-        this.setState({
-            autenticated: UserStore.isAutenticated()
-        });
+    import MyStore from '/path/to/store';
+    
+    class MyComponent extends Flux.View{
+        constructor(){
+            // initialize your state
+            this.bindStore(StoreActions);
+        }
+        
+        handleStoreChanges(data){
+            //data contains the StoreActions entire state
+        }
+    }
+```
+
+2) Or set event name and handler
+
+```
+    import MyStore from '/path/to/store';
+    
+    class MyComponent extends Flux.View{
+        constructor(){
+            // initialize your state
+            this.bindStore(StoreActions,'EVENT_NAME' function(data){
+                // data contains the store refreshed state
+            });
+        }
     }
 ```
--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import EventEmmiter from 'events';
 import { Dispatcher } from 'flux';
 
-export const FluxDispatcher = new Dispatcher();
+const FluxDispatcher = new Dispatcher();
 
-export class FluxStore extends EventEmmiter{
+class Store extends EventEmmiter{
     constructor(){
         super();
         this.state = {};
@@ -13,30 +13,64 @@ export class FluxStore extends EventEmmiter{
     
     setStoreState(newState){
         this.state = Object.assign(this.state, newState);
-        this.emit('change');
+        var isEmited = false;
+        setTimeout(()=>{
+            if(isEmited==false) throw("Warning! You need to emit the store after updating the "+this.constructor.name);
+        },1000);
+        return {
+            emit: (eventName='change') => {
+                isEmited = true;
+                this.emit('change');
+            }
+        }
     }
     
-    handleActions(actionType){
-        throw "The store must have a handleActions method";
+    handleActions(action){
+        
+        var actionSlugParts = action.actionSlug.split('.');
+        var storeName = actionSlugParts[0];
+        var storeMethod = actionSlugParts[1];
+        
+        if(storeName=='ALL' || storeName == this.constructor.name || ('_'+storeName) == this.constructor.name)
+        {
+            if(typeof(this[storeMethod]) == 'function') throw storeName+'.'+storeMethod+' must be prepended by _ (underscore) because is a "private" method';
+            else if(typeof(this['_'+storeMethod]) == 'undefined') throw storeName+' must have a method _'+storeMethod;
+            else if(typeof(this['_'+storeMethod]) != 'function') throw storeName+'._'+storeMethod+' is not a function';
+            
+            this['_'+storeMethod](action.actionData);
+        }
     }
 }
 
-export class FluxComponent extends React.Component{
+class View extends React.Component{
         
-        constructor(){
-            super();
+        constructor(props){
+            super(props);
             this._stores = [];
         }
         
-        bindStore(storeClass){
-            if(typeof(this.handleStoreChanges) == 'undefined') throw "The component must have a handleStoreChanges method";
+        bindStore(storeClass, second=null, callback=null){
+            
+            if(storeClass.constructor.name == 'ALL') throw 'Constructor cannot be called ALL, it is a reserved word';
+            
+            var eventName = 'change';
+            if(typeof second == 'string')
+            {
+                if(callback==null) throw "You need to specify the third parameter (as a function) for the bindStore call on "+storeClass;
+                eventName = second;
+            }
+            else if(typeof eventName == 'function') callback = eventName;
+
+            if(callback==null && typeof(this.handleStoreChanges) == 'undefined') throw this.constructor.name+" have a handleStoreChanges method because is binded to "+storeClass;
+            else callback = this.handleStoreChanges;
             
             if(typeof(storeClass) == 'undefined') throw "Undefined storeClass when calling setStore";
             
-            if(storeClass instanceof React.Component) storeClass = [storeClass];
+            if(storeClass instanceof Store) storeClass = [storeClass];
+            else if(typeof storeClass != 'array') throw typeof(storeClass)+" needs to be a Flux.Store class or an array of FluxStore classes";
             
             storeClass.forEach((item) => {
-                item.on('change', this.handleStoreChanges.bind(this))
+                item.on(eventName, callback.bind(this))
             });
             this._stores = this._stores.concat(storeClass);
         }
@@ -47,3 +81,23 @@ export class FluxComponent extends React.Component{
             });
         }
 }
+
+class Action{
+        
+        dispatch(actionSlug, actionData=null){
+            var actionSlugParts = actionSlug.split('.');
+            
+            if(actionSlugParts.length != 2) throw 'Action type '+actionSlug+' is invalid, you need to specify Store.method';
+            //this will be the store name
+            //var storeName = actionSlugParts[0];
+            
+            //this will be the store Method
+            //var storeMethod = actionSlugParts[1];
+            
+            FluxDispatcher.dispatch({ actionSlug, actionData });
+        }
+        
+}
+
+var Flux = { Store: Store, Action: Action, View: View }
+export default Flux;
