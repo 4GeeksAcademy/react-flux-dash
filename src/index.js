485ad65e9af68e33ffcacdd46eb641adf0af685c
Angel Lacret <alacret@gmail.com>
2018-03-27 00:32:53 -0400

    FIX: removeListener when unmounting component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import {Dispatcher} from 'flux';
 
 const FluxDispatcher = new Dispatcher();
 
+const log = function (msg) {
+    if (typeof(window.DEBUG) === 'undefined')
+        return;
+    if (window.DEBUG === true)
+        console.log(msg);
+    return
+}
+
 /**
  * Store extension that register with the FluxDispatcher singleton
  * and handles all the events
@@ -56,61 +64,66 @@ class Store extends EventEmmiter {
 }
 
 class View extends React.Component {
-        
-        constructor(props){
-            super(props);
-            this._stores = [];
-            this._callbacks = {};
-        }
 
-        /**
-         * Bind the Component to different Events from a store
-         * @param storeObject The Store object where we want to bind the event
-         * @param second The event Name that we want to bind
-         * @param callback an Optinal CallBack for the event
-         */
-        bindStore(storeClass, second = null, callback = null) {
+    constructor(props) {
+        super(props);
+        this._callbacks = [];
+    }
 
-            if (typeof(storeClass) === 'undefined') throw new Error("Undefined StoreClass when calling bindStore on "+this.constructor.name);
+    /**
+     * Bind the Component to different Events from a store
+     * @param storeObject The Store object where we want to bind the event
+     * @param second The event Name that we want to bind
+     * @param callback an Optinal CallBack for the event
+     */
+    bindStore(storeClass, second = null, callback = null) {
+        log(`Flux.View:bindStore: ${storeClass}, ${second}, ${callback}`)
 
-            if(storeClass.constructor.name === 'ALL') throw new Error("StoreClass cannot be called 'ALL', it is a reserved word");
+        if (typeof(storeClass) === 'undefined')
+            throw new Error("Undefined StoreClass when calling bindStore on " + this.constructor.name);
 
-            var eventName = 'change';
-            if(typeof second === 'string') eventName = second;
-            else if(typeof second === 'function') callback = second;
-            
-            if(callback === null && typeof(this.handleStoreChanges) === 'undefined') throw new Error(this.constructor.name+" needs to have a handleStoreChanges method or callback because is binded to a Store");
-            
-            if(callback === null) callback = this.handleStoreChanges;
-            
-            if(storeClass instanceof Store) storeClass = [storeClass];
-            else if(!Array.isArray(storeClass)) throw new Error("You are binding "+this.constructor.name+" to "+storeClass.constructor.name+" and it needs to be binded to Flux.Store classes");
+        if (storeClass.constructor.name === 'ALL')
+            throw new Error("StoreClass cannot be called 'ALL', it is a reserved word");
 
-            storeClass.forEach((item) => {
-                if (!(item instanceof Store))
-                    throw new Error(`${item} must instance of Store in ${this.constructor.name}`);
+        let eventName = 'change';
+        if (typeof second === 'string')
+            eventName = second;
 
-                if(typeof(this._callbacks[item.constructor.name]) === 'undefined')
-                    this._callbacks[item.constructor.name] = [];
-                this._callbacks[item.constructor.name].push({
-                    callbackEvent: eventName,
-                    callbackFunction: callback
-                });
-                item.on(eventName, callback.bind(this));
+        if (typeof second === 'function')
+            callback = second;
+
+        if (callback === null && typeof(this.handleStoreChanges) === 'undefined')
+            throw new Error(this.constructor.name + " needs to have a handleStoreChanges method or callback because is binded to a Store");
+
+        if (callback === null)
+            callback = this.handleStoreChanges;
+
+        if (storeClass instanceof Store)
+            storeClass = [storeClass];
+        else if (!Array.isArray(storeClass)) throw new Error("You are binding " + this.constructor.name + " to " + storeClass.constructor.name + " and it needs to be binded to Flux.Store classes");
+
+        storeClass.forEach((item) => {
+            if (!(item instanceof Store))
+                throw new Error(`${item} must instance of Store in ${this.constructor.name}`);
+
+            const realCallback = callback.bind(this);
+
+            this._callbacks.push({
+                store: item,
+                callbackEvent: eventName,
+                callbackFunction: realCallback
             });
-            this._stores = this._stores.concat(storeClass);
-        }
 
-        componentWillUnmount(){
-            this._stores.forEach((item) => {
-                if(Array.isArray(this._callbacks[item.constructor.name]))
-                {
-                    this._callbacks[item.constructor.name].forEach((callback) => {
-                        item.removeListener(callback.callbackEvent, callback.callbackFunction);
-                    });
-                }
-            });
-        }
+            item.on(eventName, realCallback);
+        });
+    }
+
+    componentWillUnmount() {
+        log("Flux.View:componentWillUnmount")
+        this._callbacks.forEach((obj) => {
+            obj.store.removeListener(obj.callbackEvent, obj.callbackFunction);
+        });
+    }
 }
 
 class Action {
